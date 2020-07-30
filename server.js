f4341893b79471319a6e34c59ff9d5a08b06d07d
phmx17 <slimyelow@gmail.com>
2020-07-30 12:18:57 +0200

    initial commit with process environments

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv'); // toold to prcess env variables; see config folder
+
+// load env vars
+dotenv.config({ path: './config/config.env' });
+console.log(process.env.NODE_ENV)
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running in: ${process.env.NODE_ENV} mode on port: ${PORT}`));
